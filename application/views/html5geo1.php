07319397f6ff8e93bba7bdec68d22afd96b69061
Dmitry Karpov <lbvf50@gmail.com>
2013-02-08 03:11:39 +0700

    geolocation error handling

--- application/views/html5geo1.php
+++ application/views/html5geo1.php
@@ -32,22 +32,36 @@
         <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
         <script>
             $(function(){ 
-                if("geolocation" in navigator){
-                    navigator.geolocation.getCurrentPosition(function(position){
-                        console.log(position);
-                        $('#success').show().text( "lat: " + position.coords.latitude + " log: " + position.coords.longitude);
-                    });
-                }else{
-                    $('#error').show().text("Gelocation not supported.");
+                var errorCallBack = function (error){
+                    // When we select "not now" the gelocaion error dose not fires;
+                    // http://screencast.com/t/wFruUQHAH
+                    // but in Chrome Fires
+                    //console.log(error);
+                    $('#error').show().text(error.message);
+                    $('#success').hide();
+                };
+                var get_geolocation = function() {
+                    if("geolocation" in navigator){
+                        navigator.geolocation.getCurrentPosition(function(position){
+                            $('#success').show().text( "lat: " + position.coords.latitude + " log: " + position.coords.longitude);
+                            $('#error').hide();     
+                        },errorCallBack);
+                    }else{
+                        $('#error').show().text("Gelocation not supported.");
+                        $('#success').hide();
+                    }
                 }
+                $("#get_geolocation").click(get_geolocation);
             });
         </script>
     </head>
     <body>
+        <h3>HTML5 geolocation (example1):</h3>
+        <button id="get_geolocation">Get geolocation</button>
         <div id="success" style="display: none;"></div>
         <div id="error" style="display: none;"></div>
-        
+
         <hr/>
-        <?php echo anchor(array("webrtc","index"),"back");?>
+        <?php echo anchor(array("webrtc", "index"), "back"); ?>
     </body>
 </html>
